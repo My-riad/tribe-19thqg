1773983ee0a09643f2e6a774258b489b50cf3abb
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:42:55 -0400

    Add public-samples/tribe-19thqg/src/web/src/screens/auth/LoginScreen/index.ts

---
+++ src/web/src/screens/auth/LoginScreen/index.ts
@@ -0,0 +1,7 @@
+import LoginScreen from './LoginScreen'; // Import the LoginScreen component for re-export
+
+/**
+ * Index file that exports the LoginScreen component as the default export,
+ * serving as the entry point for importing the LoginScreen component throughout the application.
+ */
+export default LoginScreen;
