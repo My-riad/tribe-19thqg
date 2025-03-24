a43379e1ef9700ff82ca3bc11b54461a394428e5
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:40:27 -0400

    Add public-samples/tribe-19thqg/src/web/src/components/tribe/TribeCreationForm/index.ts

---
+++ src/web/src/components/tribe/TribeCreationForm/index.ts
@@ -0,0 +1,5 @@
+import TribeCreationForm from './TribeCreationForm';
+import { TribeCreationFormProps } from './TribeCreationForm';
+
+export { TribeCreationForm };
+export type { TribeCreationFormProps };
