3935b92da9e84db12e8a1c8e6c5c78f03a98ebba
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:45:06 -0400

    Add public-samples/tribe-19thqg/src/web/src/screens/settings/PrivacySettingsScreen/index.ts

---
+++ src/web/src/screens/settings/PrivacySettingsScreen/index.ts
@@ -0,0 +1,9 @@
+import PrivacySettingsScreen from './PrivacySettingsScreen'; // react-native v0.70.0
+
+/**
+ * Index file that exports the PrivacySettingsScreen component for use throughout the application.
+ * This file serves as the entry point for importing the PrivacySettingsScreen component.
+ */
+
+// Export the PrivacySettingsScreen component as the default export to make it available to other parts of the application
+export default PrivacySettingsScreen;
