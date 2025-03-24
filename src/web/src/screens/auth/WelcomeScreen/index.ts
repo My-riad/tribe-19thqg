a7fb47217fe500da36621bcb2b93ba4d79a8ea97
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:43:08 -0400

    Add public-samples/tribe-19thqg/src/web/src/screens/auth/WelcomeScreen/index.ts

---
+++ src/web/src/screens/auth/WelcomeScreen/index.ts
@@ -0,0 +1,3 @@
+import WelcomeScreen from './WelcomeScreen'; // Import the WelcomeScreen component for re-export
+// Re-export the WelcomeScreen component as the default export
+export default WelcomeScreen;
