6e73a958b93bed9711bbe20fe5ef7162c1cbc3c2
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:44:53 -0400

    Add public-samples/tribe-19thqg/src/web/src/screens/settings/AutoMatchingPreferencesScreen/index.ts

---
+++ src/web/src/screens/settings/AutoMatchingPreferencesScreen/index.ts
@@ -0,0 +1,6 @@
+import AutoMatchingPreferencesScreen from './AutoMatchingPreferencesScreen';
+
+/**
+ * Exports the AutoMatchingPreferencesScreen component as the default export to make it available to other parts of the application
+ */
+export default AutoMatchingPreferencesScreen;
