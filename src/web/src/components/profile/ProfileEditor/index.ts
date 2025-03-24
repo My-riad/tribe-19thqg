2baff92b0d7de75e165134aa440f82841beb8186
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:39:47 -0400

    Add public-samples/tribe-19thqg/src/web/src/components/profile/ProfileEditor/index.ts

---
+++ src/web/src/components/profile/ProfileEditor/index.ts
@@ -0,0 +1,5 @@
+// Import the ProfileEditor component for re-export
+import ProfileEditor from './ProfileEditor'; // src/web/src/components/profile/ProfileEditor/ProfileEditor.tsx
+
+// Export the ProfileEditor component as the default export to make it available for import by other components
+export default ProfileEditor;
