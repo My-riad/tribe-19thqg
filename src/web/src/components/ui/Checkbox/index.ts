a5474ba8897b7341b0b3c44b3690334919e9b0ce
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:41:03 -0400

    Add public-samples/tribe-19thqg/src/web/src/components/ui/Checkbox/index.ts

---
+++ src/web/src/components/ui/Checkbox/index.ts
@@ -0,0 +1,6 @@
+import Checkbox from './Checkbox';
+import { CheckboxProps } from './Checkbox';
+import { CheckboxSize } from './Checkbox.styles';
+
+export { CheckboxProps, CheckboxSize };
+export default Checkbox;
