59dd6c373ff8ff6e29e49c9bb94977b1a6ab47a8
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:39:34 -0400

    Add public-samples/tribe-19thqg/src/web/src/components/profile/PersonalityAssessment/index.ts

---
+++ src/web/src/components/profile/PersonalityAssessment/index.ts
@@ -0,0 +1,12 @@
+import React from 'react'; // react v18.0.0
+import PersonalityAssessment, { PersonalityAssessmentProps } from './PersonalityAssessment'; // Import the main PersonalityAssessment component
+
+/**
+ * Export the PersonalityAssessment component as the default export
+ */
+export default PersonalityAssessment;
+
+/**
+ * Export the props interface for component consumers
+ */
+export type { PersonalityAssessmentProps };
