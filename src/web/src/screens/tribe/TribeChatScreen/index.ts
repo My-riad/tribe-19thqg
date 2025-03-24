86c8cdc5366203e586b1aa37d270fd38bf833d03
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:45:34 -0400

    Add public-samples/tribe-19thqg/src/web/src/screens/tribe/TribeChatScreen/index.ts

---
+++ src/web/src/screens/tribe/TribeChatScreen/index.ts
@@ -0,0 +1,15 @@
+import TribeChatScreen from './TribeChatScreen';
+
+/**
+ * @file src/web/src/screens/tribe/TribeChatScreen/index.ts
+ * @description Index file that exports the TribeChatScreen component as the default export, serving as the entry point for the TribeChatScreen module.
+ * @module TribeChatScreen
+ */
+
+/**
+ * @exports TribeChatScreen
+ * @type {React.ComponentType}
+ * @description The TribeChatScreen component, providing the UI for tribe chat functionality.
+ * @see TribeChatScreen
+ */
+export default TribeChatScreen;
