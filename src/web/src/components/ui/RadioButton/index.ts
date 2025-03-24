37e6c641bcc86d7ea2b81d0b33f77e4b4c241e5b
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:41:41 -0400

    Add public-samples/tribe-19thqg/src/web/src/components/ui/RadioButton/index.ts

---
+++ src/web/src/components/ui/RadioButton/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Entry point for the RadioButton component
+ * 
+ * This file exports the RadioButton component and its related types
+ * for use throughout the application.
+ */
+
+import RadioButton, { RadioButtonProps } from './RadioButton';
+import { RadioButtonSize } from './RadioButton.styles';
+
+export default RadioButton;
+export { RadioButtonProps, RadioButtonSize };
