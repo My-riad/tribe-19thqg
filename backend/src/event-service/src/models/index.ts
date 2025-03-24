cad33cccfd534fd008bef756072c52d515902ed3
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:28:26 -0400

    Add public-samples/tribe-19thqg/backend/src/event-service/src/models/index.ts

---
+++ backend/src/event-service/src/models/index.ts
@@ -0,0 +1,9 @@
+import EventModel from './event.model';
+import AttendeeModel from './attendee.model';
+import VenueModel from './venue.model';
+
+export {
+  EventModel,
+  AttendeeModel,
+  VenueModel
+};
