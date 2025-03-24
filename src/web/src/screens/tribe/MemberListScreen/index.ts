d8a5daf1fe209288f0d71f292e0b7e586a279429
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:45:27 -0400

    Add public-samples/tribe-19thqg/src/web/src/screens/tribe/MemberListScreen/index.ts

---
+++ src/web/src/screens/tribe/MemberListScreen/index.ts
@@ -0,0 +1,5 @@
+// Import the main MemberListScreen component
+import MemberListScreen from './MemberListScreen';
+
+// Export the MemberListScreen component as the default export for use in navigation
+export default MemberListScreen;
