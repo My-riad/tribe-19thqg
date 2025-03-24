9b960118e691aaf894ac3b786a92660635a981f0
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:40:21 -0400

    Add public-samples/tribe-19thqg/src/web/src/components/tribe/TribeChat/index.ts

---
+++ src/web/src/components/tribe/TribeChat/index.ts
@@ -0,0 +1,4 @@
+import TribeChat from './TribeChat'; // Import the TribeChat component for re-export
+// Export the TribeChat component as the default export for easier imports throughout the application
+export default TribeChat;
+export type { TribeChatProps } from './TribeChat';
