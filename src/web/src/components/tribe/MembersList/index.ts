fc7138f65059326086640fd7eb06de526931b595
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:40:08 -0400

    Add public-samples/tribe-19thqg/src/web/src/components/tribe/MembersList/index.ts

---
+++ src/web/src/components/tribe/MembersList/index.ts
@@ -0,0 +1,9 @@
+// Import the main MembersList component
+import MembersList from './MembersList';
+// Import the props interface for the MembersList component
+import { MembersListProps } from './MembersList';
+
+// Export the MembersList component as the default export for use in tribe-related screens
+export default MembersList;
+// Export the props interface for type checking when using the MembersList component
+export type { MembersListProps };
