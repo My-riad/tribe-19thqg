665355ec1cc2dffcf9e32e6fb8a1f050812c6411
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:29:18 -0400

    Add public-samples/tribe-19thqg/backend/src/matching-service/src/controllers/index.ts

---
+++ backend/src/matching-service/src/controllers/index.ts
@@ -0,0 +1,9 @@
+import CompatibilityController from './compatibility.controller';
+import MatchingController from './matching.controller';
+import SuggestionController from './suggestion.controller';
+
+export {
+  CompatibilityController,
+  MatchingController,
+  SuggestionController
+};
