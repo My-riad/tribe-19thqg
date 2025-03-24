902ab7ad2f45e556c7a96fc0f91d38a0704b4c4e
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:44:59 -0400

    Add public-samples/tribe-19thqg/src/web/src/screens/settings/NotificationSettingsScreen/index.ts

---
+++ src/web/src/screens/settings/NotificationSettingsScreen/index.ts
@@ -0,0 +1,7 @@
+import NotificationSettingsScreen from './NotificationSettingsScreen'; // Import the NotificationSettingsScreen component for re-export
+
+/**
+ * Index file that exports the NotificationSettingsScreen component for use throughout the application.
+ * This file serves as the entry point for importing the NotificationSettingsScreen component.
+ */
+export default NotificationSettingsScreen; // Export the NotificationSettingsScreen component as the default export to make it available to other parts of the application
