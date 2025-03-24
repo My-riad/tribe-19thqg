7bf0607d0962bc114a5195a1a170234af4979c59
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:43:23 -0400

    Add public-samples/tribe-19thqg/src/web/src/screens/event/EventDetailScreen/index.ts

---
+++ src/web/src/screens/event/EventDetailScreen/index.ts
@@ -0,0 +1,6 @@
+import EventDetailScreen from './EventDetailScreen'; // Import the EventDetailScreen component for re-export
+
+/**
+ * Exports the EventDetailScreen component as the default export for easy importing by other modules
+ */
+export default EventDetailScreen;
