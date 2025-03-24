d22cee69443a9890a1ff621d716a196d7945c78c
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:27:17 -0400

    Add public-samples/tribe-19thqg/backend/src/engagement-service/src/controllers/index.ts

---
+++ backend/src/engagement-service/src/controllers/index.ts
@@ -0,0 +1,18 @@
+import challengeController from './challenge.controller';
+import engagementController from './engagement.controller';
+import promptController from './prompt.controller';
+
+/**
+ * Export the challenge controller for handling challenge-related HTTP requests
+ */
+export { challengeController };
+
+/**
+ * Export the engagement controller for handling engagement-related HTTP requests
+ */
+export { engagementController };
+
+/**
+ * Export the prompt controller for handling prompt-related HTTP requests
+ */
+export { promptController };
