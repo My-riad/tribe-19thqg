e98ed4269f846430002baba9cad1cb8ae1137c7d
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:43:36 -0400

    Add public-samples/tribe-19thqg/src/web/src/screens/event/EventSuggestionScreen/index.ts

---
+++ src/web/src/screens/event/EventSuggestionScreen/index.ts
@@ -0,0 +1,7 @@
+import EventSuggestionScreen from './EventSuggestionScreen';
+
+/**
+ * Exports the EventSuggestionScreen component as the default export,
+ * serving as the entry point for the EventSuggestionScreen module.
+ */
+export default EventSuggestionScreen;
