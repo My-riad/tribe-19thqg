0a1acbc86435ee2cf1102311260689abba581f19
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:43:02 -0400

    Add public-samples/tribe-19thqg/src/web/src/screens/auth/RegistrationScreen/index.ts

---
+++ src/web/src/screens/auth/RegistrationScreen/index.ts
@@ -0,0 +1,5 @@
+// Import the RegistrationScreen component
+import RegistrationScreen from './RegistrationScreen';
+
+// Export the RegistrationScreen component as the default export
+export default RegistrationScreen;
