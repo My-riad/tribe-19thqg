4692f4daa03e5345441a53875ad0d52d612e49b7
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:40:29 -0400

    Add public-samples/tribe-19thqg/src/web/src/components/ui/index.ts

---
+++ src/web/src/components/ui/index.ts
@@ -0,0 +1,65 @@
+// Import all UI components and their types
+import Avatar, { AvatarGroup, AvatarProps, AvatarGroupProps } from './Avatar';
+import Badge, { BadgeProps, BadgeSize, BadgeVariant, BadgeType } from './Badge';
+import Button, { ButtonProps, ButtonVariant, ButtonSize } from './Button';
+import Card, { CardProps } from './Card';
+import Checkbox, { CheckboxProps } from './Checkbox';
+import Dialog, { DialogProps, DialogSize, DialogVariant, DialogAnimationType, DialogActionButton } from './Dialog';
+import Input, { InputProps, InputVariant, InputSize } from './Input';
+import ListItem, { ListItemProps, ListItemVariant, ListItemSize, ListItemDensity, ListItemPosition } from './ListItem';
+import LoadingIndicator, { LoadingIndicatorProps } from './LoadingIndicator';
+import ProgressBar, { ProgressBarProps } from './ProgressBar';
+import RadioButton, { RadioButtonProps, RadioButtonSize } from './RadioButton';
+import Slider, { SliderProps, SliderVariant, SliderSize } from './Slider';
+import TabBar, { TabItem, TabBarProps } from './TabBar';
+
+// Re-export everything for convenient imports elsewhere in the application
+export {
+  Avatar,
+  AvatarGroup,
+  AvatarProps,
+  AvatarGroupProps,
+  Badge,
+  BadgeProps,
+  BadgeSize,
+  BadgeVariant,
+  BadgeType,
+  Button,
+  ButtonProps,
+  ButtonVariant,
+  ButtonSize,
+  Card,
+  CardProps,
+  Checkbox,
+  CheckboxProps,
+  Dialog,
+  DialogProps,
+  DialogSize,
+  DialogVariant,
+  DialogAnimationType,
+  DialogActionButton,
+  Input,
+  InputProps,
+  InputVariant,
+  InputSize,
+  ListItem,
+  ListItemProps,
+  ListItemVariant,
+  ListItemSize,
+  ListItemDensity,
+  ListItemPosition,
+  LoadingIndicator,
+  LoadingIndicatorProps,
+  ProgressBar,
+  ProgressBarProps,
+  RadioButton,
+  RadioButtonProps,
+  RadioButtonSize,
+  Slider,
+  SliderProps,
+  SliderVariant,
+  SliderSize,
+  TabBar,
+  TabItem,
+  TabBarProps
+};
