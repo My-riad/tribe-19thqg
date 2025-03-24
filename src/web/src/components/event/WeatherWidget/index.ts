0633415b6db339556e64d0486f31d31626aeca9c
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:39:13 -0400

    Add public-samples/tribe-19thqg/src/web/src/components/event/WeatherWidget/index.ts

---
+++ src/web/src/components/event/WeatherWidget/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Export the WeatherWidget component for easy importing throughout the application.
+ * This barrel file simplifies imports by allowing consumers to import directly from 
+ * the component folder rather than specifying the exact file.
+ */
+export { default, WeatherWidgetProps } from './WeatherWidget';
