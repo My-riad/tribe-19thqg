e1b945d3eea2d95e9388d02160ed393bc2263114
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:41:09 -0400

    Add public-samples/tribe-19thqg/src/web/src/components/ui/Dialog/index.ts

---
+++ src/web/src/components/ui/Dialog/index.ts
@@ -0,0 +1,17 @@
+import Dialog, {
+  DialogProps,
+  DialogSize,
+  DialogVariant,
+  DialogAnimationType,
+  DialogActionButton
+} from './Dialog';
+
+export type {
+  DialogProps,
+  DialogSize,
+  DialogVariant,
+  DialogAnimationType,
+  DialogActionButton
+};
+
+export default Dialog;
