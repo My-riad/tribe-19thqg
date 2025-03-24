b50e721a29c469a06a85fe87e94f0b2b7087e371
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:43:50 -0400

    Add public-samples/tribe-19thqg/src/web/src/screens/main/DiscoverScreen/index.ts

---
+++ src/web/src/screens/main/DiscoverScreen/index.ts
@@ -0,0 +1,6 @@
+import DiscoverScreen from './DiscoverScreen'; // Import the DiscoverScreen component for re-export
+
+/**
+ * Re-export the DiscoverScreen component as the default export for use in navigation and other parts of the application
+ */
+export default DiscoverScreen;
