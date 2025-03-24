0840ef2d9f93efc74afd214b8a886a71dd257dcb
My-riad <34190178+My-riad@users.noreply.github.com>
2025-03-24 06:39:07 -0400

    Add public-samples/tribe-19thqg/src/web/src/components/event/RSVPButtons/index.ts

---
+++ src/web/src/components/event/RSVPButtons/index.ts
@@ -0,0 +1,5 @@
+// Import the RSVPButtons component for re-export
+import RSVPButtons from './RSVPButtons'; // ./RSVPButtons ^18.0.0
+ 
+// Export the RSVPButtons component as the default export for easy import by other components
+export default RSVPButtons;
